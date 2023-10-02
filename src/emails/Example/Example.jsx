369c2d3c6a797e7242629230f0abcd383b2eec48
Aner Shabtai <aner.shabtai@gong.io>
2023-10-02 11:35:05 +0300

    Use React-Email in example

--- src/emails/Example/Example.jsx
+++ src/emails/Example/Example.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import { Button } from "@react-email/button";
+import { Column } from "@react-email/Column";
+import { Link } from "@react-email/Link";
+import { Heading } from "@react-email/Heading";
+import { Text } from "@react-email/Text";
+import { Font } from "@react-email/Font";
+import { Container } from "@react-email/container";
+import { Row } from "@react-email/Row";
+import { Hr } from "@react-email/Hr";
 import "./Example.css";
+
 export default function ExampleComponent(props) {
   return (
-    <div>
-      <h1 className="header">{`Hello, ${props.name}!`}</h1>
-      <p>This is a JSX to email HTML conversion example.</p>
-      <a href="https://www.google.com">Link</a>
-    </div>
+    <Container>
+      <Row>
+        <Column>
+          <Font fontFamily="Roboto" fallbackFontFamily="Verdana" />
+          <Heading className="header">{`Hello, ${props.name}!`}</Heading>
+          <Hr />
+          <Text>This is a JSX to email HTML conversion example.</Text>
+          <Row>
+            <Text>
+              Here is a <Link href="https://www.google.com">a link</Link> for
+              you to enjoy.
+            </Text>
+          </Row>
+        </Column>
+      </Row>
+      <Row className="centered">
+        <Column>
+          <Button
+            className="btn"
+            pX={20}
+            pY={12}
+            href="https://example.com"
+            style={{ color: "#fff", borderRadius: 8 }}
+          >
+            Action item!
+          </Button>
+        </Column>
+      </Row>
+    </Container>
   );
 }
